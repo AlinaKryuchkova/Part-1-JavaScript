24c802c72094ceb1d9b5810f520a5a5379756c0a
Alina <zuzikevich1990@gmail.com@>
2023-04-16 11:35:50 -0300

    lessonn19

---
+++ study.js
@@ -0,0 +1,10 @@
+/*
+const answer = [];
+answer[0] = prompt('Сколько вам лет?', '');
+answer[1] = prompt('Как вас зовут?', '');
+answer[2] = prompt('Из какого вы города?', '');
+*/
+/*
+const category = 'toy';
+console.log('barby' + category);
+*/
